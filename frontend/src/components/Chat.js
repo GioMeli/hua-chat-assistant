a55a234acd34b985b58507c321c18ee40b21b2b9
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-03-12 22:45:21 +0200

    Update Chat.js

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -1,1 +1,13 @@
-
+import React, { useState } from "react";
+import axios from "axios";
+const Chat = () => {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const sendMessage = async () => {
+    const res = await axios.post("http://localhost:5000/chat", { message });
+    setMessages([...messages, { user: "You", text: message }, { user: "Bot", text: res.data.reply }]);
+    setMessage("");
+  };
+  return (<div>/* Chat UI */</div>);
+};
+export default Chat;
