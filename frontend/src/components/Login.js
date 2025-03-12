20f49d3fbcbc734e0c552ebe933ecd3124f9eb9c
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-03-12 22:59:38 +0200

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,1 +1,21 @@
+import React, { useState } from "react";
+import axios from "axios";
 
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    const res = await axios.post("http://localhost:5000/api/login", { email, password });
+    localStorage.setItem("token", res.data.token);
+  };
+
+  return (
+    <div>
+      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+};
+export default Login;
