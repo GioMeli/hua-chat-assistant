32ccd1f9c1f1ac99729daac5c339757c4f73db34
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-03-12 22:44:54 +0200

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,1 +1,3 @@
-
+import { initializeApp } from "firebase/app";
+const firebaseConfig = { apiKey: "YOUR_API_KEY", authDomain: "your-app.firebaseapp.com" };
+export const app = initializeApp(firebaseConfig);
