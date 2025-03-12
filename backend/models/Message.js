cb34ca6a0d45dc00d31afdc023327f1e00677882
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-03-12 22:59:08 +0200

    Update Message.js

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -1,1 +1,9 @@
-
+class Message {
+  constructor(userId, message, reply, timestamp) {
+    this.userId = userId;
+    this.message = message;
+    this.reply = reply;
+    this.timestamp = timestamp;
+  }
+}
+module.exports = Message;
