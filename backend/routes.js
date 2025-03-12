aaebb46e49ae492d1bfa12c108bcbf9ee7e90d43
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-03-12 22:46:19 +0200

    Update routes.js

--- backend/routes.js
+++ backend/routes.js
@@ -1,1 +1,11 @@
+const express = require("express");
+const { signup, login } = require("./controllers/authController");
+const { chat } = require("./controllers/chatController");
 
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/chat", chat);
+
+module.exports = router;
