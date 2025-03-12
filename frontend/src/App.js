f9ad259ea1467f7e093d27647989deba19f518c2
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-03-12 23:01:30 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ChatPage from "./pages/ChatPage";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
+const App = () => (
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/chat" element={<ChatPage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  </Router>
+);
+export default App;
