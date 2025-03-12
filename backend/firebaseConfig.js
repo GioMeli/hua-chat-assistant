3dd62a4d0b4aed4eb2bf26b924e0e54ad8fa91af
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-03-12 23:46:44 +0200

    Update firebaseConfig.js

--- backend/firebaseConfig.js
+++ backend/firebaseConfig.js
@@ -1,7 +1,11 @@
 const admin = require("firebase-admin");
 const serviceAccount = require("./serviceAccountKey.json");
 
-admin.initializeApp({ credential: admin.credential.cert(serviceAccount) });
-const db = admin.firestore();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://hua-chat-assistant-default-rtdb.firebaseio.com",  
+});
+
+const db = admin.database();  
 
 module.exports = { admin, db };
