3a9106a4ee1110261d230b6bf7b6ba1ae42a0ee3
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-03-12 23:01:54 +0200

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,1 +1,4 @@
-
+import axios from "axios";
+export const login = (data) => axios.post("http://localhost:5000/api/login", data);
+export const signup = (data) => axios.post("http://localhost:5000/api/signup", data);
+export const sendMessage = (data) => axios.post("http://localhost:5000/api/chat", data);
