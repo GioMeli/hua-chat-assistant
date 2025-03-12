1bc17448771d514c65d4f4a8e3ac3a8b607ea693
GioMeli <136624687+GioMeli@users.noreply.github.com>
2025-03-12 22:58:48 +0200

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,9 @@
-
+class User {
+  constructor(id, username, email, password) {
+    this.id = id;
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
+}
+module.exports = User;
